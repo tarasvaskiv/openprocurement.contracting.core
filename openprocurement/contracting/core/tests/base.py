593a9906c7d66b2b0e570af80ba243a3705468c2
annawzz <annawzz@quintagroup.org>
2018-04-06 11:17:17 +0300

    Remove docs

--- openprocurement/contracting/core/tests/base.py
+++ openprocurement/contracting/core/tests/base.py
@@ -11,43 +11,6 @@ from openprocurement.contracting.api.tests.base import (
 )
 
 
-class DumpsTestAppwebtest(TestApp):
-    """ Class used for docs (move to common later?) """
-    hostname = "api-sandbox.openprocurement.org"
-
-    def do_request(self, req, status=None, expect_errors=None):
-        req.headers.environ["HTTP_HOST"] = self.hostname
-        if hasattr(self, 'file_obj') and not self.file_obj.closed:
-            self.file_obj.write(req.as_bytes(True))
-            self.file_obj.write("\n")
-            if req.body:
-                try:
-                    self.file_obj.write(
-                            'DATA:\n' + json.dumps(json.loads(req.body), indent=2, ensure_ascii=False).encode('utf8'))
-                    self.file_obj.write("\n")
-                except:
-                    pass
-            self.file_obj.write("\n")
-        resp = super(DumpsTestAppwebtest, self).do_request(req, status=status, expect_errors=expect_errors)
-        if hasattr(self, 'file_obj') and not self.file_obj.closed:
-            headers = [(n.title(), v)
-                       for n, v in resp.headerlist
-                       if n.lower() != 'content-length']
-            headers.sort()
-            self.file_obj.write(str('Response: %s\n%s\n') % (
-                resp.status,
-                str('\n').join([str('%s: %s') % (n, v) for n, v in headers]),
-            ))
-
-            if resp.testbody:
-                try:
-                    self.file_obj.write(json.dumps(json.loads(resp.testbody), indent=2, ensure_ascii=False).encode('utf8'))
-                except:
-                    pass
-            self.file_obj.write("\n\n")
-        return resp
-
-
 test_contract_data = {
     u"items": [
         {
